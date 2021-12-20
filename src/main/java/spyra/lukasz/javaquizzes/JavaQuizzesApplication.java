933d629d4cd6397c4b98726af7a77d6e8d7cd708
lukaszspyra <lukasz.spyrka@gmail.com>
2021-12-20 19:13:25 +0100

    initial commit

---
+++ src/main/java/spyra/lukasz/javaquizzes/JavaQuizzesApplication.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.javaquizzes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuizzesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaQuizzesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spyra/lukasz/javaquizzes/JavaQuizzesApplicationTests.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.javaquizzes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuizzesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
