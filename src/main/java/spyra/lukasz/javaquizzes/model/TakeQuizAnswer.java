74471196832b4471fd24286af02102a158f9446e
lukaszspyra <lukasz.spyrka@gmail.com>
2021-12-25 17:18:29 +0100

    Create TakeQuizAnswer Entity

---
+++ src/main/java/spyra/lukasz/javaquizzes/model/TakeQuizAnswer.java
@@ -0,0 +1,25 @@
+package spyra.lukasz.javaquizzes.model;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Transactional
+@Entity
+@Table(name = "take_quiz_answers")
+public class TakeQuizAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int score;
+
+    @NotNull
+    private LocalDateTime start;
+
+    @NotNull
+    private LocalDateTime finish;
+
+}
