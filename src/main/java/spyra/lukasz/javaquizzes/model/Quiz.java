1e2d127216e37d5e867b77862ad2488d3b322991
lukaszspyra <lukasz.spyrka@gmail.com>
2021-12-30 09:36:39 +0100

    Create QuizMapper from Entity to DTO

--- src/main/java/spyra/lukasz/javaquizzes/model/Quiz.java
+++ src/main/java/spyra/lukasz/javaquizzes/model/Quiz.java
@@ -37,4 +37,43 @@ public class Quiz {
     @Valid
     private List<Question> questions = new ArrayList<>();
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
 }
---
+++ src/main/java/spyra/lukasz/javaquizzes/quizselector/QuizMapper.java
@@ -0,0 +1,28 @@
+package spyra.lukasz.javaquizzes.quizselector;
+
+import org.springframework.stereotype.Component;
+import spyra.lukasz.javaquizzes.model.Quiz;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class QuizMapper {
+
+    List<QuizView> toView(List<Quiz> quizzes) {
+        return quizzes.stream()
+                .map(this::toView)
+                .collect(Collectors.toList());
+    }
+
+    QuizView toView(Quiz quizz) {
+        QuizView view = new QuizView();
+        view.setId(quizz.getId());
+        view.setTitle(quizz.getTitle());
+        view.setScore(quizz.getScore());
+        view.setCreated(quizz.getCreated());
+        view.setUpdated(quizz.getUpdated());
+        return view;
+    }
+
+}
