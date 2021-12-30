2b1c66183b1a9a88f0592d92166fe4ffacc358fd
lukaszspyra <lukasz.spyrka@gmail.com>
2021-12-30 09:35:56 +0100

    Create QuizView DTO

---
+++ src/main/java/spyra/lukasz/javaquizzes/quizselector/QuizView.java
@@ -0,0 +1,56 @@
+package spyra.lukasz.javaquizzes.quizselector;
+
+import java.time.LocalDateTime;
+
+public class QuizView {
+
+    private long id;
+
+    private String title;
+
+    private int score;
+
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+}
