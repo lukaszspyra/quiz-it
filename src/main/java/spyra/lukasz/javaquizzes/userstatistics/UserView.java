edac3fdb09e377961524c5218cd1ecdda9b43089
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-01 10:07:02 +0100

    Create UserView as DTO

---
+++ src/main/java/spyra/lukasz/javaquizzes/userstatistics/UserView.java
@@ -0,0 +1,18 @@
+package spyra.lukasz.javaquizzes.userstatistics;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserView {
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
