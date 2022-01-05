0a18a6dcd444b9eb5e0348d9ec761b8ceb74a41a
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-05 09:15:43 +0100

    Create MyUserPrincipal to wrap User object for Spring Security login

---
+++ src/main/java/spyra/lukasz/javaquizzes/login/MyUserPrincipal.java
@@ -0,0 +1,69 @@
+package spyra.lukasz.javaquizzes.login;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import spyra.lukasz.javaquizzes.model.TakeQuiz;
+import spyra.lukasz.javaquizzes.userstatistics.repository.User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+class MyUserPrincipal implements UserDetails {
+
+    private User user;
+
+    public MyUserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return user.getName();
+    }
+
+    public List<TakeQuiz> getTakenQuizzes() {
+        return user.getTakenQuizzes();
+    }
+
+    public long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName());
+        return Collections.singleton(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
