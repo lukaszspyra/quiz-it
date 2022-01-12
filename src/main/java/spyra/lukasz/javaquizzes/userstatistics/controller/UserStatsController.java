6bfd6127b13def29396f7924830a3f31e1e091b1
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-12 10:08:58 +0100

    Add authentication for UserStatsController
    Authentication object injected to check for data saved in session by Spring

--- src/main/java/spyra/lukasz/javaquizzes/userstatistics/controller/UserStatsController.java
+++ src/main/java/spyra/lukasz/javaquizzes/userstatistics/controller/UserStatsController.java
@@ -1,9 +1,11 @@
 package spyra.lukasz.javaquizzes.userstatistics.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import spyra.lukasz.javaquizzes.userlogin.MyUserPrincipal;
 import spyra.lukasz.javaquizzes.userstatistics.service.StatsService;
 
 import java.security.Principal;
@@ -15,8 +17,10 @@ class UserStatsController {
     private StatsService userService;
 
     @GetMapping("/")
-    public String home(Principal principal, Model model) {
+    public String home(Principal principal, Authentication authentication, Model model) {
         model.addAttribute("user", userService.getUserStatistics(principal.getName()));
+        //TODO: for research purposes: check user data saved in session after modification in database
+        MyUserPrincipal test = (MyUserPrincipal) authentication.getPrincipal();
         return "home";
     }
 
