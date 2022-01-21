3259292566ad9d742db300076900cc3001792edc
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-21 09:49:39 +0100

    Add list of remaining question to session
    Student answers questions which are removed from the list and saved to session

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 @Controller
 class AttemptController {
 
@@ -13,9 +16,24 @@ class AttemptController {
     private AttemptService attemptService;
 
     @GetMapping("/quiz/start/{id}")
-    public String startSingleQuiz(Model model, @PathVariable long id) {
-        model.addAttribute("quiz", attemptService.getQuizById((id)));
+    public String startSingleQuiz(Model model, @PathVariable long id, HttpSession session) {
+        List<QuestionView> questions = getQuestionsFromSession(id, session);
+        if (questions.isEmpty()) {
+            return "result";
+        }
+        model.addAttribute("quiz_id", id);
+        model.addAttribute("question", questions.remove(0));
+        session.setAttribute("questions", questions);
         return "attempt";
+    }
+
+    @SuppressWarnings("unchecked cast")
+    private List<QuestionView> getQuestionsFromSession(long id, HttpSession session) {
+        Object sessionQuestions = session.getAttribute("questions");
+        if (sessionQuestions == null) {
+            return attemptService.getQuizById(id).getQuestions();
+        }
+        return (List<QuestionView>) sessionQuestions;
     }
 
 }
