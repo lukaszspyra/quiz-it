52fcf25dac6b7ded2d26ba872ed6c91b25f14d42
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-24 09:26:02 +0100

    Add creation of TakeQuiz entity to start quiz controller

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import spyra.lukasz.javaquizzes.shared.TakeQuiz;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -20,9 +23,11 @@ class AttemptController {
 
 
     @GetMapping("/quiz/start/{quiz_id}")
-    ModelAndView startSingleQuiz(Model model, @PathVariable(name = "quiz_id") long quizId, HttpSession session) {
+    ModelAndView startSingleQuiz(Principal principal, Model model, @PathVariable(name = "quiz_id") long quizId, HttpSession session) {
+        TakeQuiz takeQuiz = attemptService.takeQuiz(quizId, principal.getName());
         List<QuestionView> questions = attemptService.getQuizQuestionsRandomOrder(quizId);
         session.setAttribute("questions", questions);
+        session.setAttribute("take_quiz_id", takeQuiz.getId());
         return new ModelAndView("redirect:/quiz/attempt/"+quizId);
     }
 
@@ -37,15 +42,11 @@ class AttemptController {
         }
         model.addAttribute("quiz_id", quizId);
         model.addAttribute("question", questions.remove(0));
-        session.setAttribute("questions", questions);
         return "attempt";
     }
 
     @PostMapping("/quiz/answer/{quiz_id}")
-    ModelAndView givenAnswers(@RequestParam(value = "given_answers", required = false) long[] answers, @PathVariable(value = "quiz_id") long quizId, Model model){
-
-
-
+    ModelAndView givenAnswers(@RequestParam(value = "given_answers", required = false) Long[] answers, @PathVariable(value = "quiz_id") long quizId, Model model){
 
         return new ModelAndView("redirect:/quiz/attempt/"+quizId );
     }
