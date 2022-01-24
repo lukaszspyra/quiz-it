9edca22d4cc87768d04daa6dd9b78f88b1d84e13
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-24 09:23:24 +0100

    Create method to persist TakeQuiz entity for quiz start

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptService.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptService.java
@@ -1,12 +1,15 @@
 package spyra.lukasz.javaquizzes.feature.quizattempt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import spyra.lukasz.javaquizzes.feature.quizselector.QuizRepository;
-import spyra.lukasz.javaquizzes.shared.Question;
+import spyra.lukasz.javaquizzes.shared.*;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 class AttemptService {
@@ -20,6 +23,15 @@ class AttemptService {
     @Autowired
     private QuestionMapper questionMapper;
 
+    @Autowired
+    private TakeQuizRepository takeQuizRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private TakeQuizAnswerRepository takeQuizAnswerRepository;
+
     AttemptView getQuizById(long id) {
         return attemptMapper.toView(quizRepository.getById(id));
     }
@@ -30,4 +42,14 @@ class AttemptService {
         return questionMapper.toView(questions);
     }
 
+
+    public TakeQuiz takeQuiz(long quizId, String userEmail) {
+        Quiz presentQuiz = quizRepository.getById(quizId);
+        User activeUser = takeQuizRepository.findUserByEmail(userEmail).orElseThrow(() -> new UsernameNotFoundException("Not found"));
+        TakeQuiz takeQuiz = new TakeQuiz();
+        takeQuiz.setQuiz(presentQuiz);
+        takeQuiz.setStart(LocalDateTime.now());
+        takeQuiz.setUser(activeUser);
+        return takeQuizRepository.save(takeQuiz);
+    }
 }
---
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/TakeQuizRepository.java
@@ -0,0 +1,17 @@
+package spyra.lukasz.javaquizzes.feature.quizattempt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import spyra.lukasz.javaquizzes.shared.TakeQuiz;
+import spyra.lukasz.javaquizzes.shared.User;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+interface TakeQuizRepository extends JpaRepository<TakeQuiz, Long> {
+
+    @Query("select u from User u join fetch u.role join fetch u.takenQuizzes where u.email = ?1")
+    Optional<User> findUserByEmail(@NotNull @Email String email);
+
+}
