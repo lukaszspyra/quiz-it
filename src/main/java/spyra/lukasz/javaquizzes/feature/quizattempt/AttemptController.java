3bab1cd3bbf931ce055755be0674c5e031e60e2b
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-25 09:28:54 +0100

    Create methods for receiving form with user quiz progress
    Session attribute take_quiz_id replaced by hidden html input field, which is injected into AttemptController

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptController.java
@@ -3,10 +3,7 @@ package spyra.lukasz.javaquizzes.feature.quizattempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import spyra.lukasz.javaquizzes.shared.TakeQuiz;
 
@@ -23,32 +20,40 @@ class AttemptController {
 
 
     @GetMapping("/quiz/start/{quiz_id}")
-    ModelAndView startSingleQuiz(Principal principal, Model model, @PathVariable(name = "quiz_id") long quizId, HttpSession session) {
+    ModelAndView startSingleQuiz(@PathVariable(name = "quiz_id") long quizId,
+                                 Principal principal,
+                                 HttpSession session) {
         TakeQuiz takeQuiz = attemptService.takeQuiz(quizId, principal.getName());
         List<QuestionView> questions = attemptService.getQuizQuestionsRandomOrder(quizId);
         session.setAttribute("questions", questions);
-        session.setAttribute("take_quiz_id", takeQuiz.getId());
-        return new ModelAndView("redirect:/quiz/attempt/"+quizId);
+        return new ModelAndView("redirect:/quiz/attempt/" + quizId, "take_quiz_id", takeQuiz.getId());
     }
 
 
     @SuppressWarnings("unchecked cast")
     @GetMapping("/quiz/attempt/{quiz_id}")
-    String progressQuiz(Model model, @PathVariable(name = "quiz_id") long quizId, HttpSession session) {
+    String progressQuiz(@ModelAttribute("take_quiz_id") long takeQuizId,
+                        @PathVariable(name = "quiz_id") long quizId,
+                        Model model,
+                        HttpSession session) {
         List<QuestionView> questions = (List<QuestionView>) session.getAttribute("questions");
         if (questions.isEmpty()) {
             session.removeAttribute("questions");
             return "result";
         }
         model.addAttribute("quiz_id", quizId);
+        model.addAttribute("take_quiz_id", takeQuizId);
         model.addAttribute("question", questions.remove(0));
         return "attempt";
     }
 
     @PostMapping("/quiz/answer/{quiz_id}")
-    ModelAndView givenAnswers(@RequestParam(value = "given_answers", required = false) Long[] answers, @PathVariable(value = "quiz_id") long quizId, Model model){
-
-        return new ModelAndView("redirect:/quiz/attempt/"+quizId );
+    ModelAndView givenAnswers(@RequestParam(value = "given_answers", required = false) Long[] answerIds,
+                              @RequestParam(value = "question_id") long questionId,
+                              @RequestParam(value = "take_quiz_id") long takeQuizId,
+                              @PathVariable(value = "quiz_id") long quizId) {
+        attemptService.saveGivenAnswers(Arrays.asList(answerIds), takeQuizId, questionId);
+        return new ModelAndView("redirect:/quiz/attempt/" + quizId, "take_quiz_id", takeQuizId);
     }
 
 }
