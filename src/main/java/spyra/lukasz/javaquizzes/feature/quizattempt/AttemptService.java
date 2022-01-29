c0e875e8a2f39f467d98c549f1f902281eca9791
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-29 07:02:36 +0100

    Remove not required methods from AttemptService
    Functionality of the service was splitted between Starter/Progresser/Finisher

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptService.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptService.java
@@ -3,12 +3,8 @@ package spyra.lukasz.javaquizzes.feature.quizattempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spyra.lukasz.javaquizzes.feature.quizselector.QuizRepository;
-import spyra.lukasz.javaquizzes.shared.Answer;
 import spyra.lukasz.javaquizzes.shared.Question;
-import spyra.lukasz.javaquizzes.shared.TakeQuiz;
-import spyra.lukasz.javaquizzes.shared.TakeQuizAnswer;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,24 +15,11 @@ class AttemptService {
     private QuizRepository quizRepository;
 
     @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private TakeQuizRepository takeQuizRepository;
-
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    @Autowired
-    private TakeQuizAnswerRepository takeQuizAnswerRepository;
 
     List<QuestionView> getQuizQuestionsRandomOrder(long id) {
         List<Question> questions = quizRepository.getById(id).getQuestions();
         Collections.shuffle(questions);
         return questionMapper.toView(questions);
     }
-
 }
