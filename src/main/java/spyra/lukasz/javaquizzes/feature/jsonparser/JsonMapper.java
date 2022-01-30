978b32ca619084f1b0e4bd46134395eff2f1c608
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-30 07:48:42 +0100

    Create JsonMapper from file, with injected MapStruct config class

--- src/main/java/spyra/lukasz/javaquizzes/feature/jsonparser/JsonMapper.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/jsonparser/JsonMapper.java
@@ -1,16 +1,17 @@
 package spyra.lukasz.javaquizzes.feature.jsonparser;
 
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spyra.lukasz.javaquizzes.shared.Quiz;
 
 @Component
 class JsonMapper {
 
-    Quiz toEntity(QuizJson quizJson) {
-        ModelMapper modelMapper = new ModelMapper();
-        Quiz quiz = modelMapper.typeMap(QuizJson.class, Quiz.class)
-                .addMappings(mapper -> mapper.map(QuizJson::getQuestions, Quiz::setQuestions)).map(quizJson);
-        return quiz;
+    @Autowired
+    private QuizMapperMapStruct mapper;
+
+    Quiz toEntity(final QuizJson quizJson) {
+        return mapper.quizJsonToQuiz(quizJson);
     }
+
 }
