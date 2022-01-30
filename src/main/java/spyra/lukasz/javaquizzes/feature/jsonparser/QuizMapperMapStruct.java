42d27bd278289e46cb060d3f5bea6b2fe0dea7e5
lukaszspyra <lukasz.spyrka@gmail.com>
2022-01-30 07:45:43 +0100

    Replace interface by abstract class for MapStruct
    Purpose of it is to do custom mapping to bind Question with Quiz, Answers with Question entities. It will allow proper relations during saving to database.

--- src/main/java/spyra/lukasz/javaquizzes/feature/jsonparser/QuizMapperMapStruct.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/jsonparser/QuizMapperMapStruct.java
@@ -1,18 +1,39 @@
 package spyra.lukasz.javaquizzes.feature.jsonparser;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import spyra.lukasz.javaquizzes.shared.Answer;
 import spyra.lukasz.javaquizzes.shared.Question;
 import spyra.lukasz.javaquizzes.shared.Quiz;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper
-public interface QuizMapperMapStruct {
+@Mapper(componentModel = "spring")
+public abstract class QuizMapperMapStruct {
 
-    Quiz quizJsonToQuiz(QuizJson quizJson);
+    abstract Quiz quizJsonToQuiz(QuizJson quizJson);
 
-    List<Question> questionJsonToQuestion(List<QuestionJson> questionsJson);
+    abstract List<Question> questionJsonToQuestion(List<QuestionJson> questionsJson);
 
-    List<Answer> answerJsonToAnswer(List<AnswerJson> answersJson);
+    abstract List<Answer> answerJsonToAnswer(List<AnswerJson> answersJson);
+
+    @AfterMapping
+    protected void bindQuestionsToQuiz(@MappingTarget Quiz quiz) {
+        List<Question> questNotBound = quiz.getQuestions();
+        List<Question> questBound = questNotBound.stream()
+                .peek(q -> q.setQuiz(quiz))
+                .map(this::bindAnswerToQuestion)
+                .collect(Collectors.toUnmodifiableList());
+        quiz.setQuestions(questBound);
+    }
+
+    private Question bindAnswerToQuestion(final Question question) {
+        List<Answer> answersBound = question.getAnswers().stream()
+                .peek(a -> a.setQuestion(question))
+                .collect(Collectors.toUnmodifiableList());
+        question.setAnswers(answersBound);
+        return question;
+    }
 }
