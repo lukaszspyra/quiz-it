50f0ce45eb7b10af5793ff80d369f1e00371c09f
lukaszspyra <lukasz.spyrka@gmail.com>
2022-02-09 12:52:00 +0100

    Implement ResultDetailsController to return model with attempt details

--- src/main/java/spyra/lukasz/javaquizzes/feature/resultdetails/ResultDetailsController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/resultdetails/ResultDetailsController.java
@@ -1,14 +1,21 @@
 package spyra.lukasz.javaquizzes.feature.resultdetails;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 class ResultDetailsController {
 
+    @Autowired
+    private ResultDetailsService service;
 
-    @GetMapping
-    String showResultDetails() {
+    @GetMapping("/attempt/{attempt_id}/result/")
+    String showResultDetails(@PathVariable(value = "attempt_id") long attemptId,
+                             Model model) {
+        model.addAttribute(service.quizAttemptDetails(attemptId));
         return "result-details";
     }
 
