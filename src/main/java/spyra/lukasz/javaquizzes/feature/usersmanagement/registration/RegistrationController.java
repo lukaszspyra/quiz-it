b051a7e62f606cb1b187e308f599b0b0704a0191
lukaszspyra <lukasz.spyrka@gmail.com>
2022-02-18 14:03:59 +0100

    Create registerUserAccount() in RegistrationController

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/RegistrationController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/RegistrationController.java
@@ -1,15 +1,49 @@
 package spyra.lukasz.javaquizzes.feature.usersmanagement.registration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spyra.lukasz.javaquizzes.shared.AvailableRole;
+import spyra.lukasz.javaquizzes.shared.User;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
 class RegistrationController {
 
-    @GetMapping("/user/registration")
-    public String showRegistrationForm(Model model) {
-        NewUserDTO userDto = new NewUserDTO();
-        model.addAttribute("user", userDto);
+    @Autowired
+    private UserRegisterService registerService;
+
+
+    @GetMapping("/user/register")
+    public String showRegistrationForm(NewUserDTO newUserDTO) {
         return "user-registration";
     }
 
+
+    @PostMapping("/user/register")
+    public String registerUserAccount(
+            @Valid NewUserDTO newUserDTO,
+            BindingResult bindingResult,
+            Model model,
+            HttpServletRequest httpServletRequest) throws ServletException {
+        if (bindingResult.hasErrors()) {
+            return "user-registration";
+        }
+        User registered;
+        try {
+            registered = registerService.registerAccount(newUserDTO, AvailableRole.USER);
+        } catch (UserAlreadyExistsException ex) {
+            bindingResult.rejectValue("email", "newUserDTO.email", ex.getMessage());
+            return "user-registration";
+        }
+        model.addAttribute("registrationsuccess", "You have been successfully registered.");
+        httpServletRequest.login(registered.getEmail(),registered.getPassword());
+        return "/";
+    }
 }
