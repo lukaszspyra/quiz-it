2c178154ad5d6f4f37aaa8b72aad9ef9115e9e6b
lukaszspyra <lukasz.spyrka@gmail.com>
2022-02-24 22:59:49 +0100

    User REST convention naming endpoints for UserManagementController methods

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UsersManagementController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UsersManagementController.java
@@ -4,33 +4,33 @@ package spyra.lukasz.javaquizzes.feature.usersmanagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import spyra.lukasz.javaquizzes.shared.AvailableRole;
 
 @Controller
-@RequestMapping("/superadmin/management")
+@RequestMapping("/superadmin/user-management")
 class UsersManagementController {
 
     @Autowired
     private AuthorityService authorityService;
 
-    @PutMapping("/authority/admin")
+    @PutMapping("/authority/admin-role")
     String addAdminRole(@RequestParam(value = "id") long userId) {
         authorityService.changeRole(userId, AvailableRole.ADMIN);
-        return "redirect:/superadmin/management";
+        return "redirect:/superadmin/user-management/users";
     }
 
-    @PutMapping("/authority/user")
+    @PutMapping("/authority/user-role")
     String removeAdminRole(@RequestParam(value = "id") long adminId) {
+        //TODO: use Data from Optional or simplify to boolean flag
         authorityService.changeRole(adminId, AvailableRole.USER);
-        return "redirect:/superadmin/management";
+        return "redirect:/superadmin/user-management/users";
     }
 
-    @GetMapping
-    String showAllUsers(Model model) {
+
+    @GetMapping("/users")
+    String showAllUsers(@RequestParam String success,
+                        Model model) {
         model.addAllAttributes(authorityService.showAllUsersGroupedByRole());
         return "all-users";
     }
