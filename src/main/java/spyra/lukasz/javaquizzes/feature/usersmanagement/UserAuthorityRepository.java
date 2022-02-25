91480f8f86967940ab67cf5372992b4f6b67f16e
lukaszspyra <lukasz.spyrka@gmail.com>
2022-02-25 22:50:50 +0100

    Add deleteUserById() to UserAuthorityRepository

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UserAuthorityRepository.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UserAuthorityRepository.java
@@ -4,5 +4,9 @@ package spyra.lukasz.javaquizzes.feature.usersmanagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spyra.lukasz.javaquizzes.shared.User;
 
+import java.util.Optional;
+
 interface UserAuthorityRepository extends JpaRepository<User, Long> {
+
+    Optional<User> deleteUserById(long idToDelete);
 }
