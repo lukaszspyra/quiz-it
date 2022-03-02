23e48a2278d294d6f2f57aedd05e04c32fde0bc3
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-02 20:30:42 +0100

    Add success role changed message as RedirectAttribute

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UsersManagementController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UsersManagementController.java
@@ -1,12 +1,13 @@
 package spyra.lukasz.javaquizzes.feature.usersmanagement;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spyra.lukasz.javaquizzes.shared.AvailableRole;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/superadmin/user-management")
@@ -16,15 +17,18 @@ class UsersManagementController {
     private UserManagementService userManagementService;
 
     @PutMapping("/authority/admin-role")
-    String addAdminRole(@RequestParam(value = "id") long userId) {
-        userManagementService.changeRole(userId, AvailableRole.ADMIN);
+    String addAdminRole(@RequestParam(value = "id") long userId,
+                        RedirectAttributes redirectAttributes) {
+        final Optional<ChangeUserRoleDTO> roleChanged = userManagementService.changeRole(userId, AvailableRole.ADMIN);
+        roleChanged.ifPresent(newAdmin -> redirectAttributes.addFlashAttribute("message", "Admin role set for learner: " + newAdmin.getEmail()));
         return "redirect:/superadmin/user-management/users";
     }
 
     @PutMapping("/authority/user-role")
-    String removeAdminRole(@RequestParam(value = "id") long adminId) {
-        //TODO: use Data from Optional or simplify to boolean flag
-        userManagementService.changeRole(adminId, AvailableRole.USER);
+    String removeAdminRole(@RequestParam(value = "id") long adminId,
+                           RedirectAttributes redirectAttributes) {
+        final Optional<ChangeUserRoleDTO> roleChanged = userManagementService.changeRole(adminId, AvailableRole.USER);
+        roleChanged.ifPresent(newUser -> redirectAttributes.addFlashAttribute("message", "User role set for learner: " + newUser.getEmail()));
         return "redirect:/superadmin/user-management/users";
     }
 
