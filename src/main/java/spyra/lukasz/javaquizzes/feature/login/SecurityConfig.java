425b2d1ed85e05c32956e6ea2625e2b9a29eec61
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-13 08:53:43 +0100

    Enable PostAuthorize for methods

--- src/main/java/spyra/lukasz/javaquizzes/feature/login/SecurityConfig.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/login/SecurityConfig.java
@@ -2,6 +2,7 @@ package spyra.lukasz.javaquizzes.feature.login;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final String[] resources = new String[]{
