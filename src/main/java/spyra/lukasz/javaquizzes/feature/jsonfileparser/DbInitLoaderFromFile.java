5291287e777af65685836594ecd48737df3670ea
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-21 08:24:14 +0100

    Read all files with quizzes from given path

--- src/main/java/spyra/lukasz/javaquizzes/feature/jsonfileparser/DbInitLoaderFromFile.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/jsonfileparser/DbInitLoaderFromFile.java
@@ -1,17 +1,19 @@
 package spyra.lukasz.javaquizzes.feature.jsonfileparser;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import spyra.lukasz.javaquizzes.shared.Quiz;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @Component
 class DbInitLoaderFromFile {
 
-    private static final Path FILE_PATH = Path.of("src/main/resources/quizzcontent/free/Linux.json");
+    private static final Path FILES_PATH = Path.of("src/main/resources/quizzcontent/free");
     public static final String PREDEFINED = "Predefined";
 
     @Autowired
@@ -25,8 +27,16 @@ class DbInitLoaderFromFile {
 
     @PostConstruct
     void loadQuizzesFromFile() throws IOException {
-        final QuizJson quizJson = jsonReader.readJsonFile(FILE_PATH);
-        final Quiz quiz = jsonMapper.toEntity(quizJson);
-        quizRepo.save(quiz);
+        try (Stream<Path> files = Files.list(FILES_PATH)){
+            files
+                    .map(this::getQuizFromJson)
+                    .map(json -> jsonMapper.toEntity(json))
+                    .forEach(quizRepo::save);
+        }
+    }
+
+    @SneakyThrows
+    private QuizJson getQuizFromJson(Path file) {
+        return jsonReader.readJsonFile(file);
     }
 }
