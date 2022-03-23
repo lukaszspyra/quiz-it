fafe78912d745b576790ae48106993a66331690a
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-23 08:45:29 +0100

    Create uri with api_key from properties to fetch random quizzes

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizselector/ApiService.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizselector/ApiService.java
@@ -1,18 +1,48 @@
 package spyra.lukasz.javaquizzes.feature.quizselector;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 class ApiService {
 
-    private HttpRequest createRequest(String uri) {
+    private static final String API_URL = "https://quizapi.io/api/v1/questions?apiKey=";
+
+    @Autowired
+    private PropertiesReader propertiesReader;
+
+    String getRandomQuizFromApi(final String tag, final String difficulty) throws IOException, InterruptedException {
+        String uri = generateUri(tag, difficulty);
+        final HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(createRequest(uri), HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    private HttpRequest createRequest(final String uri) {
         return HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .GET()
                 .build();
     }
+
+    private String generateUri(final String tag, final String difficulty) throws IOException {
+        final StringBuilder builder = new StringBuilder();
+        return builder.append(API_URL)
+                .append(propertiesReader.readProperty("api_key"))
+                .append("&")
+                .append(propertiesReader.readProperty("questions_number"))
+                .append("&difficulty=")
+                .append(difficulty)
+                .append("t&ags=")
+                .append(tag)
+                .toString();
+    }
+
 
 }
