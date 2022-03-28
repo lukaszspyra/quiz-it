044a719a6348039fedac46499234c265cadcb0c3
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-28 10:07:11 +0200

    Replace difficulty by number of questions in random API quiz
    API does not provide quizzes for all difficulties and 20 questions quizzes seem too long.

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizcreator/apiparser/ApiService.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizcreator/apiparser/ApiService.java
@@ -32,30 +32,28 @@ class ApiService {
     private QuizInitRepository repository;
 
     /**
-     * Gets {@link Quiz} form API call, based on tag and difficulty choosen by the user.
+     * Gets {@link Quiz} form API call, based on tag and questionNumber chosen by the user.
      *
      * @param tag        Category tag of the quiz
-     * @param difficulty of the quiz
+     * @param questionNumber of the quiz
      * @return parsed and saved quiz from database
      * @throws IOException          when {@link java.util.Properties} file is not found
      * @throws InterruptedException when there is API connection issue with sending request
      */
-    Quiz getRandomQuizFromApi(final String tag, final String difficulty) throws IOException, InterruptedException {
-        String uri = generateUri(tag, difficulty);
+    Quiz getRandomQuizFromApi(final String tag, final String questionNumber) throws IOException, InterruptedException {
+        String uri = generateUri(tag, questionNumber);
         final HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(createRequest(uri), HttpResponse.BodyHandlers.ofString());
         final Quiz quizFromApi = jsonReader.parseApiJson(response.body());
         return repository.save(quizFromApi);
     }
 
-    private String generateUri(final String tag, final String difficulty) throws IOException {
+    private String generateUri(final String tag, final String questionNumber) throws IOException {
         final StringBuilder builder = new StringBuilder();
         return builder.append(API_URL)
                 .append(propertiesReader.readProperty("api_key", "settings.properties"))
-                .append("&")
-                .append(propertiesReader.readProperty("questions_number", "settings.properties"))
-                .append("&difficulty=")
-                .append(difficulty)
+                .append("&limit=")
+                .append(questionNumber)
                 .append("&tags=")
                 .append(tag)
                 .toString();
