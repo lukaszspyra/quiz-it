2af18d355684df45a5f5196edff8fb6d97691945
lukaszspyra <lukasz.spyrka@gmail.com>
2022-03-29 10:17:04 +0200

    Replace websocket @PathParam by spring's @RequestParam

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizcreator/apiparser/ApiController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizcreator/apiparser/ApiController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import spyra.lukasz.javaquizzes.shared.Quiz;
 
-import javax.websocket.server.PathParam;
 import java.io.IOException;
 
 /**
@@ -27,15 +27,15 @@ class ApiController {
      * Passes required parameters for creating random quiz. Result database id is returned to view controller
      *
      * @param tag        describes category for the generated quiz
-     * @param questions for the generated quiz
+     * @param questionsNumber for the generated quiz
      * @return redirects to AttemptController with quiz id from database
      * @throws IOException          when {@link java.util.Properties} file is not found
      * @throws InterruptedException when there is API connection issue with sending request
      */
     @PostMapping("/quizzes/random")
-    String generateRandomQuiz(@PathParam(value = "tag") String tag,
-                              @PathParam(value = "questions") String questions) throws IOException, InterruptedException {
-        final Quiz createdQuiz = apiService.getRandomQuizFromApi(tag, questions);
+    String generateRandomQuiz(@RequestParam(value = "tag") String tag,
+                              @RequestParam(value = "questions") String questionsNumber) throws IOException, InterruptedException {
+        final Quiz createdQuiz = apiService.getRandomQuizFromApi(tag, questionsNumber);
         return "redirect:/quiz/" + createdQuiz.getId();
     }
 }
