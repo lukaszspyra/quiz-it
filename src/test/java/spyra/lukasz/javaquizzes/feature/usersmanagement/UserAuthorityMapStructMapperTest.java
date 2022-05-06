d93fade0a2c849570aab2c479da668cc1ae2557c
lukaszspyra <lukasz.spyrka@gmail.com>
2022-05-06 08:24:15 +0200

    Create UserAuthorityMapStructMapperTest

---
+++ src/test/java/spyra/lukasz/javaquizzes/feature/usersmanagement/UserAuthorityMapStructMapperTest.java
@@ -0,0 +1,22 @@
+package spyra.lukasz.javaquizzes.feature.usersmanagement;
+
+import org.testng.annotations.Test;
+import spyra.lukasz.javaquizzes.shared.User;
+
+import static org.testng.Assert.*;
+
+@Test(groups = "Mapper")
+public class UserAuthorityMapStructMapperTest {
+
+    @Test(description = "Shall return proper DTO, but it did not", dataProvider = "usersWithCorrespondingAuthorityDTOs", dataProviderClass = UserManagementDataProvider.class)
+    public void shallMapUserToAuthorityChangeDTO(User user, ChangeUserRoleDTO dto) {
+        //given
+        UserAuthorityMapStructMapper mapper = new UserAuthorityMapStructMapperImpl();
+
+        //when
+        final ChangeUserRoleDTO result = mapper.toView(user);
+
+        //then
+        assertEquals(dto, result);
+    }
+}
