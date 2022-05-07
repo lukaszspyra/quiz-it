6c2037b248ae86141b8d4fdc3bebb9fe7380721b
lukaszspyra <lukasz.spyrka@gmail.com>
2022-05-07 06:38:46 +0200

    Create AttemptMapperTest

---
+++ src/test/java/spyra/lukasz/javaquizzes/feature/quizattempt/AttemptMapperTest.java
@@ -0,0 +1,24 @@
+package spyra.lukasz.javaquizzes.feature.quizattempt;
+
+import org.testng.annotations.Test;
+import spyra.lukasz.javaquizzes.shared.Quiz;
+
+import static org.testng.Assert.assertEquals;
+
+@Test(groups = "Mapper")
+public class AttemptMapperTest {
+
+    @Test(description = "Shall return proper DTO, but it did not", dataProvider = "quizzesWithCorrespondingAttemptDTOs", dataProviderClass = AttemptDataProvider.class)
+    public void shallMapUserToAuthorityChangeDTO(Quiz quiz, AttemptView dto) {
+        //given
+        AnswerMapper answerMapper = new AnswerMapper();
+        QuestionMapper questionMapper = new QuestionMapper(answerMapper);
+        AttemptMapper mapper = new AttemptMapper(questionMapper);
+
+        //when
+        final AttemptView result = mapper.toView(quiz);
+
+        //then
+        assertEquals(dto, result);
+    }
+}
