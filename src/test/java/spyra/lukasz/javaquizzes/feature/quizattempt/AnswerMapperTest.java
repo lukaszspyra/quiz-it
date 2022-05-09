8ca691a3b094bdc54607df843a85fa401b476fa9
lukaszspyra <lukasz.spyrka@gmail.com>
2022-05-09 08:32:18 +0200

    Add unhappy path for AnswerMapperTest

--- src/test/java/spyra/lukasz/javaquizzes/feature/quizattempt/AnswerMapperTest.java
+++ src/test/java/spyra/lukasz/javaquizzes/feature/quizattempt/AnswerMapperTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import spyra.lukasz.javaquizzes.shared.Answer;
 
+import static org.testng.Assert.assertNotEquals;
+
 @Test(groups = "Mapper")
 public class AnswerMapperTest {
 
@@ -21,6 +23,18 @@ public class AnswerMapperTest {
         softAssert.assertEquals(resultView.isCorrect(), dto.isCorrect());
         softAssert.assertEquals(resultView.getContent(), dto.getContent());
         softAssert.assertAll("Shall have the same id, correctness and contents,");
+    }
+
+    @Test(description = "Shall fail to return proper DTO, but it did not.", dataProvider = "answersWithRandomDTOs", dataProviderClass = AttemptDataProvider.class)
+    public void shallNotMapAnswerToRandomView(Answer answer, AnswerView dto) {
+        //given
+        AnswerMapper mapper = new AnswerMapper();
+
+        //when
+        final AnswerView result = mapper.toView(answer);
+
+        //then
+        assertNotEquals(result, dto);
     }
 
 }
