c872c9335fb97343d04dda3971f5621dad645468
lukaszspyra <lukasz.spyrka@gmail.com>
2022-05-12 08:24:42 +0200

    Add String length validator to NewUserDTO

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/NewUserDTO.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/NewUserDTO.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Component;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * DTO for new {@link spyra.lukasz.javaquizzes.shared.User} registration.
- *
+ * <p>
  * All data is validated by javax validators and custom {@link PasswordMatches} validating annotation
  */
 @Component
@@ -21,19 +22,23 @@ class NewUserDTO {
 
     @NotNull(message = "Name can not be null")
     @NotEmpty(message = "Name can not be empty")
+    @Size(min = 2, max = 70, message = "Name must be between 2 and 70 characters")
     private String name;
 
     @NotNull(message = "Email can not be null")
     @NotEmpty(message = "Email can not be empty")
     @Email
+    @Size(min = 2, max = 50, message = "Email must be between 2 and 50 characters")
     private String email;
 
     @NotNull(message = "Password can not be null")
     @NotEmpty(message = "Password can not be empty")
+    @Size(min = 2, max = 50, message = "Password must be between 2 and 50 characters")
     private String password;
 
     @NotNull(message = "Matching password can not be null")
     @NotEmpty(message = "Matching password can not be empty")
+    @Size(min = 2, max = 50, message = "Password must be between 2 and 50 characters")
     private String matchingPassword;
 
 }
