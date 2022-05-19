4f769e5b6a58cd8c36307362824fa40500313285
lukaszspyra <lukasz.spyrka@gmail.com>
2022-05-19 07:35:43 +0200

    Add error test view with controller
    MyController overwrites default /error view resolver

---
+++ src/main/java/spyra/lukasz/javaquizzes/feature/error/MyErrorController.java
@@ -0,0 +1,21 @@
+package spyra.lukasz.javaquizzes.feature.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("code",status);
+        return "error";
+    }
+}
--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/UserAlreadyExistsException.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/UserAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package spyra.lukasz.javaquizzes.feature.usersmanagement.registration;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.CONFLICT, reason="User already exists")
 class UserAlreadyExistsException extends Exception{
     UserAlreadyExistsException(String message) {
         super(message);
