d29ce42e76240f300ea8c2e9098d904ebbe86b8c
lukaszspyra <lukasz.spyrka@gmail.com>
2022-06-05 16:32:40 +0200

    Add mappings to progressQuiz() in order to use entities behaviour

--- src/main/java/spyra/lukasz/javaquizzes/feature/demo/DemoProgresser.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/demo/DemoProgresser.java
@@ -19,35 +19,25 @@ class DemoProgresser {
 
     private final ScoreCounter scoreCounter;
 
-    /**
-     * Make progress of demo quiz attempt by updating {@link TakeQuiz} with given list of {@link Answer}
-     *
-     * As it is demo feature, previous answers are not saved, but score is updated.
-     *
-     * @param question    during attempt
-     * @param markedIds   selected answers Ids
-     * @param currentDemo current {@link TakeQuiz} attempt instance
-     */
-    void progressQuiz(final Question question, final List<Long> markedIds, final TakeQuiz currentDemo) {
-        final List<Answer> answered = question.selectAnswers(markedIds);
-        final TakeQuiz updatedDemo = currentDemo.progressQuizAttempt(scoreCounter.count(answered), LocalDateTime.now());
-        updatedDemo.setGivenAnswers(createGivenAnswers(answered, question, updatedDemo));
-    }
+    private final TakeDemoMapper mapper;
 
     /**
-     * Creates new complete list of {@link TakeQuizAnswer}
+     * Make progress of demo quiz attempt by updating {@link TakeQuiz} with given list of {@link Answer}
+     * <p>
+     * As it is demo feature, previous answers are not saved, but score is updated.
      *
-     * @param answered    marked answers by the user
-     * @param question    current demo question
-     * @param updatedQuiz current demo quiz attempt
-     * @return list of created {@link TakeQuizAnswer}
+     * @param questionDTO during attempt
+     * @param markedIds   selected answers Ids
+     * @param takeDemoDTO current {@link TakeQuiz} attempt instance
+     * @return demo DTO with updated score
      */
-    private List<TakeQuizAnswer> createGivenAnswers(List<Answer> answered, Question question, TakeQuiz updatedQuiz) {
-        List<TakeQuizAnswer> takeQuizAnswers = new ArrayList<>();
-        for (var answer : answered) {
-            takeQuizAnswers.add(new TakeQuizAnswer(updatedQuiz, question, answer));
-        }
-        return takeQuizAnswers;
+    TakeDemoDTO progressQuiz(final QuestionDTO questionDTO, final List<Long> markedIds, final TakeDemoDTO takeDemoDTO) {
+        final Question question = mapper.toQuestion(questionDTO);
+        final TakeQuiz currentDemo = mapper.fromDTO(takeDemoDTO);
+
+        final List<Answer> answered = question.selectAnswers(markedIds);
+        final TakeQuiz updatedDemo = currentDemo.progressQuizAttempt(scoreCounter.count(answered), LocalDateTime.now());
+        return mapper.toDTO(updatedDemo);
     }
 
 }
