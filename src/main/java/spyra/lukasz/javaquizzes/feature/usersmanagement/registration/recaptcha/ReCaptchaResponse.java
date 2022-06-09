06bd98aabeb39cdd746944b157306b978b4f63f1
lukaszspyra <lukasz.spyrka@gmail.com>
2022-06-09 10:00:16 +0200

    Add google ReCaptchaResponse class to be mapped from directly from API response

---
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/recaptcha/ReCaptchaResponse.java
@@ -0,0 +1,75 @@
+package spyra.lukasz.javaquizzes.feature.usersmanagement.registration.recaptcha;
+
+import com.fasterxml.jackson.annotation.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({
+        "success",
+        "challenge_ts",
+        "hostname",
+        "error-codes"
+})
+@Getter
+@Setter
+public class ReCaptchaResponse {
+
+    @JsonProperty("success")
+    private boolean success;
+
+    @JsonProperty("challenge_ts")
+    private String challengeTs;
+
+    @JsonProperty("hostname")
+    private String hostname;
+
+    @JsonProperty("error-codes")
+    private ErrorCode[] errorCodes;
+
+    @JsonIgnore
+    public boolean hasClientError() {
+        if(errorCodes == null) {
+            return false;
+        }
+        for(ErrorCode error : errorCodes) {
+            switch(error) {
+                case InvalidResponse:
+                case MissingResponse:
+                case BadRequest:
+                case TimeOut:
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    enum ErrorCode {
+        MissingSecret,
+        InvalidSecret,
+        MissingResponse,
+        InvalidResponse,
+        BadRequest,
+        TimeOut;
+
+        private static final Map<String, ErrorCode> errorsMap = new HashMap<>(6);
+
+        static {
+            errorsMap.put("missing-input-secret",   MissingSecret);
+            errorsMap.put("invalid-input-secret",   InvalidSecret);
+            errorsMap.put("missing-input-response", MissingResponse);
+            errorsMap.put("invalid-input-response", InvalidResponse);
+            errorsMap.put("bad-request", BadRequest);
+            errorsMap.put("timeout-or-duplicate", TimeOut);
+        }
+
+        @JsonCreator
+        public static ErrorCode createForValue(String value) {
+            return errorsMap.get(value.toLowerCase());
+        }
+    }
+}
