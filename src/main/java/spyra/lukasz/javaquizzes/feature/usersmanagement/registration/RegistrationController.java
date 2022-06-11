efb0d5ff5ab61f6e4121c497bd4911177b602395
lukaszspyra <lukasz.spyrka@gmail.com>
2022-06-11 14:40:44 +0200

    Add reCaptcha response as RequestParam to RegistrationController

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/RegistrationController.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/RegistrationController.java
@@ -1,12 +1,14 @@
 package spyra.lukasz.javaquizzes.feature.usersmanagement.registration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spyra.lukasz.javaquizzes.feature.usersmanagement.registration.recaptcha.InvalidReCaptchaException;
+import spyra.lukasz.javaquizzes.feature.usersmanagement.registration.recaptcha.ReCaptchaService;
 import spyra.lukasz.javaquizzes.shared.AvailableRole;
 
 import javax.validation.Valid;
@@ -20,6 +22,8 @@ class RegistrationController {
 
     private final UserRegisterService registerService;
 
+    private final ReCaptchaService reCaptchaService;
+
     @GetMapping("/register")
     public String showRegistrationForm(NewUserDTO newUserDTO) {
         return "register";
@@ -27,24 +31,29 @@ class RegistrationController {
 
     /**
      * Registers new User based on form data received by POST
-     *
+     * <p>
      * If there are validating errors in the filled up form, or User already exists, returns to registration with proper message.
-     * @param newUserDTO tied to DTO object {@link NewUserDTO}
+     *
+     * @param newUserDTO    tied to DTO object {@link NewUserDTO}
      * @param bindingResult results of validated data
-     * @param model passes successful registration message
+     * @param model         passes successful registration message
      * @return proper view (login/registration on success/failure of registration process)
      */
     @PostMapping("/register")
     public String registerUserAccount(
+            @RequestParam(value = "g-recaptcha-response") String captcha,
             @Valid NewUserDTO newUserDTO,
             BindingResult bindingResult,
             Model model) {
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors() || captcha == null || captcha.isBlank()) {
+            //TODO:add captcha error to model
             return "register";
         }
+
         try {
+            reCaptchaService.processResponse(captcha);
             registerService.registerAccount(newUserDTO, AvailableRole.USER);
-        } catch (UserAlreadyExistsException ex) {
+        } catch (UserAlreadyExistsException | InvalidReCaptchaException ex) {
             bindingResult.rejectValue("email", "newUserDTO.email", ex.getMessage());
             return "register";
         }
