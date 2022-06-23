4309ea6d544fd2a2d76a65be9dee0de0aa9acb89
lukaszspyra <lukasz.spyrka@gmail.com>
2022-06-23 20:53:03 +0200

    Add javadoc to ReCaptchaService

--- src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/recaptcha/ReCaptchaService.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/usersmanagement/registration/recaptcha/ReCaptchaService.java
@@ -9,6 +9,11 @@ import org.springframework.web.client.RestOperations;
 import java.net.URI;
 import java.util.regex.Pattern;
 
+/**
+ * Process reCaptcha response
+ *
+ * Response form view is validated and verified on proper google's url
+ */
 @Service
 @RequiredArgsConstructor
 public class ReCaptchaService {
@@ -19,6 +24,14 @@ public class ReCaptchaService {
 
     private static final Pattern RESPONSE_PATTERN = Pattern.compile("[A-Za-z0-9_-]+");
 
+    /**
+     * Process response from reCaptcha view module
+     *
+     * Obtain reCaptcha secret and verify url from application.properties to create post url
+     *
+     * @param response from view as String
+     * @throws InvalidReCaptchaException if reCaptcha response is not valid or not verified successfully
+     */
     public void processResponse(String response) throws InvalidReCaptchaException {
         if (!validateResponse(response)) {
             throw new InvalidReCaptchaException("ReCaptcha response contains invalid characters");
@@ -36,6 +49,14 @@ public class ReCaptchaService {
         }
     }
 
+    /**
+     * Validate reCaptcha view's response
+     *
+     * Check that the given String is neither null nor of length 0.
+     * Check for allowed characters regex from pattern {@link #RESPONSE_PATTERN}
+     * @param response as String
+     * @return true or false for valid/invalid response validation result
+     */
     private boolean validateResponse(String response) {
         return StringUtils.hasLength(response) && RESPONSE_PATTERN.matcher(response).matches();
     }
