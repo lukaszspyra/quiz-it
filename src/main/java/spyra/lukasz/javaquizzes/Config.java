4440dae240ed2ff2b12dda80bf607f6cf480bbb1
lukaszspyra <lukasz.spyrka@gmail.com>
2022-07-01 20:47:28 +0200

    Fix local config bugs
    Production profile disabled;
    Add @PropertySource in order to make settings.properties visible for Spring;
    Set hidden data for api url and key;
    Fix bug in data.sql table plural name

--- src/main/java/spyra/lukasz/javaquizzes/Config.java
+++ src/main/java/spyra/lukasz/javaquizzes/Config.java
@@ -3,13 +3,15 @@ package spyra.lukasz.javaquizzes;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestOperations;
 
 /**
  * Configuration of RestOperations factory method
  */
 @Configuration
-public class RestConfig {
+@PropertySource("/settings.properties")
+public class Config {
 
     @Bean
     public RestOperations restOperations(RestTemplateBuilder restTemplateBuilder) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO role (id, name) VALUES (1,'SUPER_ADMIN');
 INSERT INTO role (id, name) VALUES (2,'ADMIN');
 INSERT INTO role (id, name) VALUES (3, 'USER');
 
-INSERT INTO user (email, name, password, role_id) VALUES ('test0@test.pl', 'User0', '$2a$04$gb9XG47BMLY9w/jVGnSmuOThZW7Gx3hbn.V0qETeU3kWXX8ppXYt2',1);
-INSERT INTO user (email, name, password, role_id) VALUES ('test1@test.pl', 'User1', '$2a$04$gb9XG47BMLY9w/jVGnSmuOThZW7Gx3hbn.V0qETeU3kWXX8ppXYt2',2);
-INSERT INTO user (email, name, password, role_id) VALUES ('test2@test.pl', 'User2', '$2a$04$6SDR9Hm53NTCXTWqPhJWiu.XyIJeCLKZwHdzbZuFICLbNP2BymSQa',3);
+INSERT INTO users (email, name, password, role_id) VALUES ('test0@test.pl', 'User0', '$2a$04$gb9XG47BMLY9w/jVGnSmuOThZW7Gx3hbn.V0qETeU3kWXX8ppXYt2',1);
+INSERT INTO users (email, name, password, role_id) VALUES ('test1@test.pl', 'User1', '$2a$04$gb9XG47BMLY9w/jVGnSmuOThZW7Gx3hbn.V0qETeU3kWXX8ppXYt2',2);
+INSERT INTO users (email, name, password, role_id) VALUES ('test2@test.pl', 'User2', '$2a$04$6SDR9Hm53NTCXTWqPhJWiu.XyIJeCLKZwHdzbZuFICLbNP2BymSQa',3);
