52804ad8d574b1aa2e3fc14258e89c9ea2c5af50
lukaszspyra <lukasz.spyrka@gmail.com>
2022-07-07 20:39:02 +0200

    Fix bug in ResultDeleteRepository
    Parameter was not injected into @PreAuthorize due to missing '#'

--- src/main/java/spyra/lukasz/javaquizzes/feature/quizresults/delete/ResultDeleteRepository.java
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizresults/delete/ResultDeleteRepository.java
@@ -4,16 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import spyra.lukasz.javaquizzes.shared.TakeQuiz;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * Deletes {@link TakeQuiz} from logged-in {@link spyra.lukasz.javaquizzes.shared.User}
  */
 interface ResultDeleteRepository extends JpaRepository<TakeQuiz, Long> {
 
     /**
-     * Secured to check if {@link TakeQuiz} belongs to currently logged in {@link spyra.lukasz.javaquizzes.shared.User}
+     * Secured to check if {@link TakeQuiz} belongs to currently logged-in {@link spyra.lukasz.javaquizzes.shared.User}
      *
-     * @param attempt to be deleted
+     * @param attempt entity to be deleted
      */
-    @PreAuthorize("attempt.user.email == authentication.principal.getUsername()")
-    void deleteTakeQuizBy(TakeQuiz attempt);
+    @PreAuthorize("#attempt.getUser().getEmail() == authentication.principal.getUsername()")
+    void delete(@NotNull TakeQuiz attempt);
+
 }
