2a11c2c5ac98c56885d4daae3afee7392cefe468
lukaszspyra <lukasz.spyrka@gmail.com>
2022-07-07 20:40:12 +0200

    Create ResultDeleteController

---
+++ src/main/java/spyra/lukasz/javaquizzes/feature/quizresults/delete/ResultDeleteController.java
@@ -0,0 +1,29 @@
+package spyra.lukasz.javaquizzes.feature.quizresults.delete;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Deletes quiz attempt results
+ */
+@Controller
+@RequiredArgsConstructor
+class ResultDeleteController {
+
+    private final ResultRemoveService service;
+
+    /**
+     * Deletes quiz attempt result
+     *
+     * @param attemptId of entity to be deleted
+     * @return redirects to profile view controller
+     */
+    @DeleteMapping("/attempt/{attempt_id}/result/")
+    String deleteResult(@PathVariable(value = "attempt_id") long attemptId) {
+        service.deleteAttemptResult(attemptId);
+        return "redirect:/profile";
+    }
+}
