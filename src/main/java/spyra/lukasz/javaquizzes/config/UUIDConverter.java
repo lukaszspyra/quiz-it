77b9ad3465f940715c03f8ca51fade0b67be0a10
lukaszspyra <lukasz.spyrka@gmail.com>
2022-08-24 16:44:17 +0200

    Add javadoc to UUIDConverter

--- src/main/java/spyra/lukasz/javaquizzes/config/UUIDConverter.java
+++ src/main/java/spyra/lukasz/javaquizzes/config/UUIDConverter.java
@@ -1,9 +1,16 @@
-package spyra.lukasz.javaquizzes;
+package spyra.lukasz.javaquizzes.config;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.UUID;
 
+/**
+ * Heroku official docs suggest to add this class before deploying to production
+ *
+ * It is required to use UUID as primary keys in PostgreSQL on production
+ *
+ * @see <a href=https://devcenter.heroku.com/articles/preparing-a-spring-boot-app-for-production-on-heroku>Heroku Docs</a>
+ */
 @Converter(autoApply = true)
 public class UUIDConverter implements AttributeConverter<UUID, UUID> {
     @Override
