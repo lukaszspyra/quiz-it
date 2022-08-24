b2837c631cb9a0d33619db76ca0559c3d28fafcc
lukaszspyra <lukasz.spyrka@gmail.com>
2022-08-24 16:31:25 +0200

    Move RestOperationsBeanCreator to config package

--- src/main/java/spyra/lukasz/javaquizzes/config/RestOperationsBeanCreator.java
+++ src/main/java/spyra/lukasz/javaquizzes/config/RestOperationsBeanCreator.java
@@ -1,17 +1,22 @@
-package spyra.lukasz.javaquizzes;
+package spyra.lukasz.javaquizzes.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestOperations;
 
 /**
  * Configuration of RestOperations factory method
  */
 @Configuration
-public class Config {
+public class RestOperationsBeanCreator {
 
+    /**
+     * Creates bean {@link RestOperations} for retrieval post results of google's ReCaptcha verification
+     *
+     * @param restTemplateBuilder resolved automatically by Spring
+     * @return instance of RestOperations
+     */
     @Bean
     public RestOperations restOperations(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
